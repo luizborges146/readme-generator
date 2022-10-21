fcd338486f6371d5a9d276d90575e8895fb3f709
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-21 07:27:38 -0700

    Create the inquirer questions, so I can adapt the ReadMe according to the user selection

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+inquirer([
+    {
+        type:"input",
+        message:"Please, enter your name",
+        name:"test",
+    },
+    {
+        type:"input",
+        message:"Please, enter your project name",
+        name:"test",
+    },
+    {
+        type:"input",
+        message:"Plase, enter your GitHub user name",
+        name:"test",
+    },
+    {
+        type:"input",
+        message:"Please, enter your email",
+        name:"test",
+    },    {
+        type:"input",
+        message:"Any message",
+        name:"test",
+    },
+    
+])
