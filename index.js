ea8644f7761d8c2f3f96cce5ee0302649b51fc57
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-21 07:46:36 -0700

    createan fs.write function that will send the user infomration back

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
+const { deepStrictEqual } = require("assert");
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-inquirer([
-    {
-        type:"input",
-        message:"Please, enter your name",
-        name:"test",
-    },
-    {
-        type:"input",
-        message:"Please, enter your project name",
-        name:"test",
-    },
-    {
-        type:"input",
-        message:"Plase, enter your GitHub user name",
-        name:"test",
-    },
-    {
-        type:"input",
-        message:"Please, enter your email",
-        name:"test",
-    },    {
-        type:"input",
-        message:"Any message",
-        name:"test",
-    },
-    
-])
+inquirer
+    .prompt([
+        {
+            type:"input",
+            message:"Please, enter your name",
+            name:"test",
+        },
+        {
+            type:"input",
+            message:"Please, enter your project name",
+            name:"test",
+        },
+        {
+            type:"input",
+            message:"Plase, enter your GitHub user name",
+            name:"test",
+        },
+        {
+            type:"input",
+            message:"Please, enter your email",
+            name:"test",
+        },    {
+            type:"input",
+            message:"Any message",
+            name:"test",
+        }, 
+    ])
+    .then((responses)=> {
+        const readMeFile = generateREAD(responses);
+
+        fs.writeFile("README.md", generateREAD,(err) => {
+            if(err) {
+                throw err;
+            }
+            console.log("README.MD Successfully created")
+        })
+    });
+  
+
