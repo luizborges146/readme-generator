a4d179d75850deb46e950c0569e495bca08ce436
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-21 15:20:55 -0700

    Fixing a bug where the JS file added an extra space on the readme file, so it removes the README format

--- README.md
+++ README.md
@@ -1,57 +1,56 @@
 
-    # readme-generator
-    Link to [GitHub repository](https://github.com/luizborges146/readme-generator)
+# ReadMe Generator
+Link to [GitHub repository](https://github.com/luizborges146/readme-generator)
     
-    ## Table of Contents
+## Table of Contents
     
-    1.  [Description](#description)
-    2.  [Instalation](#instalation)
-    3.  [Usage Infomration](#usage)
-    4.  [External support documentation](#externalDocumentation)
-    5.  [Tests](#tests)
-    6.  [Social](#social)
-    7.  [Plugins](#plugins)
-    8.  [License](#license)
+1.  [Description](#description)
+2.  [Instalation](#instalation)
+3.  [Usage Infomration](#usage)
+4.  [External support documentation](#externalDocumentation)
+5.  [Tests](#tests)
+6.  [Social](#social)
+7.  [Plugins](#plugins)
+8.  [License](#license)
     
-    ## [Description](#description)
-    This project has a particular way to create a readme file for future projects, this also help to reduce time and infomration that once needed to be applied manually
-    
-    
-    ## [Instalation](#instalation)
+## [Description](#description)
+Create a readme generator so I dont need to spend time adding this infomration in the future.
     
     
-    ## [Usage](#usage)
+## [Instalation](#instalation)
     
     
-    ## [External support documentation](#externalDocumentation)
-    
-    - [Font Awesome](https://fontawesome.com/)<br />
-    - [W3School](https://www.w3schools.com/)<br />
-    - [Mozilla](https://developer.mozilla.org)<br />
-    - [READ.me](https://docs.readme.com/docs/linking-to-pages")<br />
-    - [GitHub](https://pages.github.com/)<br />
-    - [Git_cheat_sheet_pdf](https://education.github.com/git-cheat-sheet-education.pdf)<br />
-    - [BootsStrap](https://getbootstrap.com/docs/4.5/)<br />
-    - [jQuery](https://jquery.com/)<br />
-    - [JqueryUi](https://jqueryui.com/)<br />
-    - [MomentJs](https://momentjs.com/docs/#/displaying/format/)<br />
-    - [npm](https://www.npmjs.com/)
+## [Usage](#usage)
     
     
-    ## [Tests](#tests)
-    N/A
+## [External support documentation](#externalDocumentation)
     
-    ## [Social](#social)
-    if you need any further information or support, please, send an email to: luiz.borges.146@g.com
+- [Font Awesome](https://fontawesome.com/)<br />
+- [W3School](https://www.w3schools.com/)<br />
+- [Mozilla](https://developer.mozilla.org)<br />
+- [READ.me](https://docs.readme.com/docs/linking-to-pages")<br />
+- [GitHub](https://pages.github.com/)<br />
+- [Git_cheat_sheet_pdf](https://education.github.com/git-cheat-sheet-education.pdf)<br />
+- [BootsStrap](https://getbootstrap.com/docs/4.5/)<br />
+- [jQuery](https://jquery.com/)<br />
+- [JqueryUi](https://jqueryui.com/)<br />
+- [MomentJs](https://momentjs.com/docs/#/displaying/format/)<br />
+- [npm](https://www.npmjs.com/)
     
-    [<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg' alt='github' height='40'>](https://github.com/luizborges146) [<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg' alt='linkedin' height='40'>](undefined)
+    
+## [Tests](#tests)
+N/A
+    
+## [Social](#social)
+if you need any further information or support, please, send an email to: luiz@gmail.com
+    
+[<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg' alt='github' height='40'>](https://github.com/luizborges146) [<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg' alt='linkedin' height='40'>](undefined)
     
     
     
-    ## [Plugins](#plugins)
-    N/A
+## [Plugins](#plugins)
+N/A
     
-    ## [License](#license)
-    Created by Luiz
-    Please refer to the LICENSE in the repo.
-    
+## [License](#license)
+Created by luiz
+Please refer to the LICENSE in the repo.
--- index.js
+++ index.js
@@ -60,62 +60,62 @@ inquirer
 
 
 
-    const generateREAD = (responses) =>
-    `
-    # ${responses.projectName}
-    Link to [GitHub repository](https://github.com/${responses.githubUserName}/${responses.githubRepoName})
+const generateREAD = (responses) =>
+`
+# ${responses.projectName}
+Link to [GitHub repository](https://github.com/${responses.githubUserName}/${responses.githubRepoName})
     
-    ## Table of Contents
+## Table of Contents
     
-    1.  [Description](#description)
-    2.  [Instalation](#instalation)
-    3.  [Usage Infomration](#usage)
-    4.  [External support documentation](#externalDocumentation)
-    5.  [Tests](#tests)
-    6.  [Social](#social)
-    7.  [Plugins](#plugins)
-    8.  [License](#license)
+1.  [Description](#description)
+2.  [Instalation](#instalation)
+3.  [Usage Infomration](#usage)
+4.  [External support documentation](#externalDocumentation)
+5.  [Tests](#tests)
+6.  [Social](#social)
+7.  [Plugins](#plugins)
+8.  [License](#license)
     
-    ## [Description](#description)
-    ${responses.projectDescription}
-    
-    
-    ## [Instalation](#instalation)
+## [Description](#description)
+${responses.projectDescription}
     
     
-    ## [Usage](#usage)
+## [Instalation](#instalation)
     
     
-    ## [External support documentation](#externalDocumentation)
-    
-    - [Font Awesome](https://fontawesome.com/)<br />
-    - [W3School](https://www.w3schools.com/)<br />
-    - [Mozilla](https://developer.mozilla.org)<br />
-    - [READ.me](https://docs.readme.com/docs/linking-to-pages")<br />
-    - [GitHub](https://pages.github.com/)<br />
-    - [Git_cheat_sheet_pdf](https://education.github.com/git-cheat-sheet-education.pdf)<br />
-    - [BootsStrap](https://getbootstrap.com/docs/4.5/)<br />
-    - [jQuery](https://jquery.com/)<br />
-    - [JqueryUi](https://jqueryui.com/)<br />
-    - [MomentJs](https://momentjs.com/docs/#/displaying/format/)<br />
-    - [npm](https://www.npmjs.com/)
+## [Usage](#usage)
     
     
-    ## [Tests](#tests)
-    N/A
+## [External support documentation](#externalDocumentation)
     
-    ## [Social](#social)
-    if you need any further information or support, please, send an email to: ${responses.userEmail}
+- [Font Awesome](https://fontawesome.com/)<br />
+- [W3School](https://www.w3schools.com/)<br />
+- [Mozilla](https://developer.mozilla.org)<br />
+- [READ.me](https://docs.readme.com/docs/linking-to-pages")<br />
+- [GitHub](https://pages.github.com/)<br />
+- [Git_cheat_sheet_pdf](https://education.github.com/git-cheat-sheet-education.pdf)<br />
+- [BootsStrap](https://getbootstrap.com/docs/4.5/)<br />
+- [jQuery](https://jquery.com/)<br />
+- [JqueryUi](https://jqueryui.com/)<br />
+- [MomentJs](https://momentjs.com/docs/#/displaying/format/)<br />
+- [npm](https://www.npmjs.com/)
     
-    [<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg' alt='github' height='40'>](https://github.com/${responses.githubUserName}) [<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg' alt='linkedin' height='40'>](${responses.linkdinUrl})
+    
+## [Tests](#tests)
+N/A
+    
+## [Social](#social)
+if you need any further information or support, please, send an email to: ${responses.userEmail}
+    
+[<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg' alt='github' height='40'>](https://github.com/${responses.githubUserName}) [<img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg' alt='linkedin' height='40'>](${responses.linkdinUrl})
     
     
     
-    ## [Plugins](#plugins)
-    N/A
+## [Plugins](#plugins)
+N/A
     
-    ## [License](#license)
-    Created by ${responses.userName}
-    Please refer to the LICENSE in the repo.
-    `;
+## [License](#license)
+Created by ${responses.userName}
+Please refer to the LICENSE in the repo.
+`;
 
